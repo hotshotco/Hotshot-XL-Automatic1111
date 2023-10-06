1d910a440165687bfd190fe860dd2c71a8ea8857
John M <john.michael.mullan@gmail.com>
2023-10-06 18:47:28 +0000

    group norm overriding

--- scripts/hotshot_xl_model_controller.py
+++ scripts/hotshot_xl_model_controller.py
@@ -4,6 +4,7 @@ from sgm.modules.attention import SpatialTransformer
 from sgm.modules.diffusionmodules.openaimodel import ResBlock, TimestepEmbedSequential
 import torch.nn as nn
 from einops import rearrange
+from sgm.modules.diffusionmodules.util import GroupNorm32
 
 class InflateTime(nn.Module):
     def __init__(self, module, video_length):
@@ -18,7 +19,6 @@ class InflateTime(nn.Module):
 
 class HotshotXLModelController:
 
-
     def inject(self, spatial_module, temporal_module, type_to_insert_after: type, video_length=8):
         for i, module in enumerate(spatial_module):
             if type(module) == type_to_insert_after:
@@ -27,8 +27,6 @@ class HotshotXLModelController:
 
     def hijack_sdxl_model(self, sd_model, temporal_layers: HotshotXLTemporalLayers):
         unet = sd_model.model.diffusion_model
-
-        # todo - need to insert hooks for the down and up blocks.
 
         unet_input_block_index_to_temporal_layer_map = {
             1: {"block_index": 0, "attention_index": 0, "insert_after": ResBlock},
@@ -73,6 +71,19 @@ class HotshotXLModelController:
 
             self.inject(unet.output_blocks[unet_block_index], temporal_module, temporal_info['insert_after'])
 
+        # hotshot xl group norms with a different tensor arrangement - same as animate diff
+
+        self.gn32_original_forward = GroupNorm32.forward
+        gn32_original_forward = self.gn32_original_forward
+
+        def groupnorm32_mm_forward(self, x):
+            x = rearrange(x, "(b f) c h w -> b c f h w", b=2)
+            x = gn32_original_forward(self, x)
+            x = rearrange(x, "b c f h w -> (b f) c h w", b=2)
+            return x
+
+        GroupNorm32.forward = groupnorm32_mm_forward
+
         # modules
         # - TimestepEmbedSequential
         # -- Res block
@@ -83,6 +94,7 @@ class HotshotXLModelController:
 
     def restore(self, sd_model):
         unet = sd_model.model.diffusion_model
+        GroupNorm32.forward = self.gn32_original_forward
 
         for block in unet.input_blocks:
             if type(block) is TimestepEmbedSequential:
